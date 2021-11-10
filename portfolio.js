7e5fe786a6aac1a4e9ffb6e4efe0088611ccb13d
nemanja58 <93869241+nemanja58@users.noreply.github.com>
2021-11-10 22:44:11 +0100

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,4 @@
-$(document).ready(function() {
-    $('.video-gallery').magnificPopup({
-    delegate: 'a', 
-    type: 'iframe',
-    gallery:{
-      enabled:true
-    }
-  });
-  });
+
   
 // picture sliderjs
 
